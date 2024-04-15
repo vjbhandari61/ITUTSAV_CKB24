e2908a30376565df4f44dc109a56a6d8df59c6fe
vjbhandari61 <vjbhandari61@gmail.com>
2024-04-15 15:40:22 +0530

    ngrok config

--- client/src/utils/requester.js
+++ client/src/utils/requester.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 
-const HOST_URI = "http://localhost:8080"
+const HOST_URI = "https://bd0e-2409-4085-aec3-c255-b167-1018-b651-2227.ngrok-free.app"
+const headers = { 
+    'ngrok-skip-browser-warning':true
+}
 
 
 export async function getQuestions(){
     const QUESTIONS_FETCH_ROUTE = "api/questions/fetch"
     const ReqURI = `${HOST_URI}/${QUESTIONS_FETCH_ROUTE}`
 
-    let questions = await axios.get(ReqURI)
+    let questions = await axios.get(ReqURI, {headers})
     return questions
 }
 
@@ -17,7 +20,7 @@ export async function getTeams(){
     const TEAM_FETCH = 'api/team/fetch'
     const ReqURI = `${HOST_URI}/${TEAM_FETCH}`
 
-    let response = await axios.get(ReqURI)
+    let response = await axios.get(ReqURI, {headers})
     return response
 }
 
@@ -29,27 +32,24 @@ export async function getAdmins(adminData){
     let admins = await axios.post(ReqURI, {
         "name": adminData.name, 
         "password": adminData.password
-    })
+    }, {headers})
     if(admins.status==200)
         return true
     return false
 }
 
-
-
-
 export async function addTeam(teamData){
     const TEAM_ADD_ROUTE = 'api/team/update'
     const REQ_URI = `${HOST_URI}/${TEAM_ADD_ROUTE}`
 
-    await axios.post(REQ_URI, teamData)
+    await axios.post(REQ_URI, teamData, {headers})
 }
 
 export async function updateQuestions(questionsArr){
-    const QUESTIONS_ROUTE = 'api//questions/update'
+    const QUESTIONS_ROUTE = 'api/questions/update'
     const ReqURI = `${HOST_URI}/${QUESTIONS_ROUTE}`
 
-    await axios.post(ReqURI, questionsArr).then(res=>
+    await axios.post(ReqURI, questionsArr, {headers}).then(res=>
         console.log(res)).catch(err =>console.log(err))
 }
 
